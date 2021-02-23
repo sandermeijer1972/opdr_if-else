dff6f4bb76a457764cab85e2be937f5c022ddacf
sandermeijer1972 <69959339+sandermeijer1972@users.noreply.github.com>
2021-02-23 10:17:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var age = 23;
+var isFemale = true;
+// leeftijd en gender-check
+if(age < 18) {
+    console.log("Sorry, kom maar terug als je oud genoeg bent");
+} else if (isFemale) {
+    console.log("Kom binnen, jongedame");
+} else {
+    console.log("Sorry, vandaag is ladies-night");
+}
+// controle of mevrouw nog mag rijden
+var driverStatus = 'bob';
+if(driverStatus == 'bob') {
+    console.log("Rij voorzichtig!");
+} else {
+    console.log("Sorry, maar U heeft teveel gedronken, ik regel een taxi voor U");
+}
+
